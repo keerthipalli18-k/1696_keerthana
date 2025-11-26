d04a1215c91b59ef5406fdaad57407a48b3dc190
Keerthana_1696 <keerthipalli18@gmail.com>
2025-11-26 15:23:31 +0530

    Add files via upload

---
+++ keerthi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=3;
+        printf("a+b=%d\n",a+b);
+        printf("a-b=%d\n",a-b);
+        printf("a*b=%d\n",a*b);
+        printf("a/b=%d\n",a/b);
+        printf("a%%b=%d\n",a%b);
+        if (a>b&& b>0){
+            printf("both conditions are true\n");
+        }
+        return 0;
+    }
